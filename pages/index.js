a03daed7952b87a9e0bfd8296218705b2d3cfeeb
Dominik Kienzler <kienzler@vonaffenfels.de>
2024-01-24 14:55:12 +0100

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+export default function Index() {
+    return (
+        <div>
+            <h1>Index</h1>
+        </div>
+    )
+}
+
+export async function getStaticProps(context) {
+    return {
+        props: {
+
+        }
+    }
+}
